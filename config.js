7fa3a39f32224f150dc699656b1377474a00c448
toxicturbov <110754894+toxicturbov@users.noreply.github.com>
2023-01-01 09:25:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
+global.Owner = ['917561852787']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['917561852787']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['917561852787']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.BotName = "Stranger"
+global.packname = "stranger bot"
+global.author = "By: Turbo 777"
+global.OwnerName = "Turbo 777"
+global.BotSourceCode = "https://github.com/toxicturbov/Miku-MD" // -- Please don't change -- //
+global.SupportGroupLink = "https://chat.whatsapp.com/ITJsEdLI2WlIzsnclrEcuI"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
